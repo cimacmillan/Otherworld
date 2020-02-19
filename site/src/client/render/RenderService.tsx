5889953474eaf1ed010c21631cfa533458a075fe
Callum 🍺 <callumimacmillan@gmail.com>
2020-02-19 18:53:22 +0000

    10000 triangle moving demo

--- site/src/client/render/RenderService.tsx
+++ site/src/client/render/RenderService.tsx
@@ -6,7 +6,7 @@ import * as glm from "gl-matrix";
 import { vsSource } from "./shaders/basic/Vertex";
 import { fsSource } from "./shaders/basic/Fragment";
 
-
+const triangles = 10000;
 export class RenderService {
 
     private shaderProgram: WebGLProgram;
@@ -15,7 +15,7 @@ export class RenderService {
     private projectionMatrix: WebGLUniformLocation;
     private modelMatrix: WebGLUniformLocation;
 
-    private positions: number[];
+    private positions: Float32Array;
 
     public init(renderState: RenderState) {
 
@@ -38,30 +38,57 @@ export class RenderService {
         
 
 
-        this.positions = [];
+        const pos = [];
 
-        for (let i = 0; i < 10000; i ++ ){
-            this.positions.push(...[
+        for (let i = 0; i < triangles; i ++ ){
+            pos.push(...[
                 -1.0,  1.0, -i - 10,
                 1.0,  1.0, -i - 10,
                 -1.0, -1.0, -i - 10, 
             ]);
         }
+
+        this.positions = new Float32Array(pos);
         
         // Now pass the list of positions into WebGL to build the
         // shape. We do this by creating a Float32Array from the
         // JavaScript array, then use it to fill the current buffer.
         
         gl.bufferData(gl.ARRAY_BUFFER,
-                        new Float32Array(this.positions),
-                        gl.STATIC_DRAW);        
+            this.positions,
+            gl.DYNAMIC_DRAW);        
     }
 
+    private time = 0;
+
     public createImage(renderState: RenderState, worldState: WorldState) {
-
         const { screen } = renderState;
-
         const gl = screen.getOpenGL();
+
+        this.time += 0.05;
+
+        for (let i = 0; i < triangles; i ++ ){
+            const index = i * 3 * 3;
+            const sin = Math.sin((i / triangles) * 10 + this.time) * 10;
+
+            this.positions[index] = -1.0 + sin;
+            this.positions[index + 1] = 1.0;
+            this.positions[index + 2] = -i - 10;
+
+            this.positions[index + 3] = 1.0 + sin;
+            this.positions[index + 4] = 1.0;
+            this.positions[index + 5] = -i - 10;
+
+            this.positions[index + 6] = -1.0 + sin;
+            this.positions[index + 7] = -1.0;
+            this.positions[index + 8] = -i - 10;
+        }
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER,
+            this.positions,
+            gl.DYNAMIC_DRAW);     
+
 
         gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
         gl.clearDepth(1.0);                 // Clear everything
@@ -73,7 +100,7 @@ export class RenderService {
         const fieldOfView = 45 * Math.PI / 180;   // in radians
         const aspect = 1280 / 720;
         const zNear = 0.1;
-        const zFar = 100.0;
+        const zFar = 10000.0;
         const projectionMatrix = glm.mat4.create();
 
         // note: glmatrix.js always has the first argument
