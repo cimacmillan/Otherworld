e1b0cd4faa0a139c9d4ca77ff8cd97d3f2a30e35
Callum 🍺 <callumimacmillan@gmail.com>
2020-05-20 22:04:43 +0100

    trying to work out how items should work

--- site/src/client/services/scripting/inventory/ItemBehaviours.ts
+++ site/src/client/services/scripting/inventory/ItemBehaviours.ts
@@ -9,21 +9,40 @@ import {
 } from "../../../types/TypesItem";
 
 interface ItemBehaviour {
+    // anything with can should be an item property
     canConsume: (entity: Entity<BaseState>) => boolean;
     onConsume: (entity: Entity<BaseState>) => void;
-
-    canTraded: () => void;
+    canTraded: (entity: Entity<BaseState>) => boolean;
     tradingPrice: () => number;
 }
 
-type ItemBehaviourMap = {
-    [key in ItemType]: (component: ItemComponent) => Partial<ItemBehaviour>;
+const defaultBehaviour: ItemBehaviour = {
+    canConsume: () => false,
+    onConsume: () => undefined,
+    canTraded: () => false,
+    tradingPrice: () => 0,
 };
 
 export function getItemBehaviours(item: Item) {
     const resultingBehaviours = item.behaviours.map((itemBehaviour) =>
         itemBehaviours[itemBehaviour.type](itemBehaviour)
     );
+
+    // TODO this is long:
+    return {
+        canConsume: (entity: Entity<BaseState>) =>
+            resultingBehaviours.reduce(
+                (prev, curr) =>
+                    prev && (curr.canConsume ? curr.canConsume(entity) : true),
+                true
+            ),
+        canTraded: (entity: Entity<BaseState>) =>
+            resultingBehaviours.reduce(
+                (prev, curr) =>
+                    prev && (curr.canTraded ? curr.canTraded(entity) : true),
+                true
+            ),
+    };
 }
 
 const healsPlayerBehaviour = (item: ItemHealsPlayer) => ({
@@ -37,6 +56,10 @@ const canBeTradedBehaviour = (item: ItemCanBeTraded) => ({
     canBeTraded: () => true,
     tradingPrice: () => item.price,
 });
+
+type ItemBehaviourMap = {
+    [key in ItemType]: (component: ItemComponent) => Partial<ItemBehaviour>;
+};
 
 const itemBehaviours: ItemBehaviourMap = {
     [ItemType.HEALS_PLAYER]: healsPlayerBehaviour,
