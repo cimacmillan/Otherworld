4e3987a3fa3e8e9bf9dc9c65ef3798b1e7b94452
Callum 🍺 <callumimacmillan@gmail.com>
2021-04-30 23:14:48 +0100

    Compile art script works I think

--- site/src/scripts/CompileArt.js
+++ site/src/scripts/CompileArt.js
@@ -22,30 +22,114 @@ function writeJson(dir, json) {
     fs.writeFileSync(dir, val, { flag: "w" });
 }
 
-function insertPngFileIntoNewManifest(manifest, obj) {
-    console.log(obj);
-    return {
-        ...manifest,
-        [obj.fileName]: "test",
+function getManifestSize(manifest) {
+    let width = 0;
+    let height = 0;
+
+    Object.entries(manifest).forEach(([key, value]) => {
+        width = Math.max(value.x + value.width, width);
+        height = Math.max(value.y + value.height, height);
+    });
+
+    return [width, height];
+}
+
+function getDoRectanglesOverlap(recA, recB) {
+    if (recA.x + recA.width <= recB.x) {
+        return false;
     }
+    if (recA.x >= recB.x + recB.width) {
+        return false;
+    }
+    if (recA.y + recA.height <= recB.y) {
+        return false;
+    }
+    if (recA.y >= recB.y + recB.height) {
+        return false;
+    }
+    return true;
+}
+
+function getIsManifestValid(manifest) {
+    let isValid = true;
+    Object.entries(manifest).forEach(([key, value]) => {
+        Object.entries(manifest).forEach(([compareKey, compareValue]) => {
+            if (key === compareKey) {
+                return;
+            }
+            if (getDoRectanglesOverlap(value, compareValue)) {
+                isValid = false;
+            }
+        });
+    });
+    return isValid;
+}
+
+function insertPngFileIntoNewManifest(manifest, obj) {
+    const [ manifestWidth, manifestHeight ] = getManifestSize(manifest);
+
+    let smallestSize = undefined;
+    let bestManifest = undefined;
+
+    for (let x = 0; x < manifestWidth + 1; x++) {
+        for (let y = 0; y < manifestHeight + 1; y++) {
+            const newManifest = {
+                ...manifest,
+                [obj.fileName]: {
+                    x,
+                    y,
+                    width: obj.png.width,
+                    height: obj.png.height
+                }
+            };
+
+            if (!getIsManifestValid(newManifest)) {
+                continue;
+            }
+
+            const [ newWidth, newHeight ] = getManifestSize(newManifest);
+            const size = newWidth * newHeight;
+            if (smallestSize === undefined || size < smallestSize) {
+                smallestSize = size;
+                bestManifest = newManifest;
+            }
+        }
+    }
+
+    return bestManifest;
+}
+
+function convertManifestAndPngFilesToPng(manifest, pngFiles) {
+    const [ width, height ] = getManifestSize(manifest);
+    const pngDest = new PNG({
+        width,
+        height,
+        filterType: -1
+    });
+
+    Object.entries(manifest).forEach(([key, value]) => {
+        const { x, y, width, height } = value;
+        const png = pngFiles.find(file => file.fileName === key);
+        for (var yPix = 0; yPix < png.png.height; yPix++) {
+            for (var xPix = 0; xPix < png.png.width; xPix++) {
+
+                const destX = x + xPix;
+                const destY = y + yPix;
+                const writeIndex = (pngDest.width * destY + destX) << 2;
+                const readIndex = (png.png.width * yPix + xPix) << 2;
+                pngDest.data[writeIndex] = png.png.data[readIndex] 
+                pngDest.data[writeIndex + 1] = png.png.data[readIndex + 1]
+                pngDest.data[writeIndex + 2] = png.png.data[readIndex + 2]
+                pngDest.data[writeIndex + 3] = png.png.data[readIndex + 3]
+            }
+        }
+    })
+    return pngDest;
 }
 
 function mapPngFilesToPngAndManifest(pngFiles) {
     const manifest = pngFiles.reduce((manifest, file) => insertPngFileIntoNewManifest(manifest, file), {})
-    const pngDest = new PNG({
-        width: 100,
-        height: 100,
-        filterType: -1
-    });
-    for (var y = 0; y < pngDest.height; y++) {
-        for (var x = 0; x < pngDest.width; x++) {
-            var idx = (pngDest.width * y + x) << 2;
-            pngDest.data[idx  ] = Math.floor(Math.random() * 255); // red
-            pngDest.data[idx+1] = Math.floor(Math.random() * 255);; // green
-            pngDest.data[idx+2] = Math.floor(Math.random() * 255);; // blue
-            pngDest.data[idx+3] = 255; // alpha (0 is transparent)
-        }
-    }
+    const pngDest = convertManifestAndPngFilesToPng(manifest, pngFiles);
     return [pngDest, manifest]
 }
 
