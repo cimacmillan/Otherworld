a7ac85b4f10995f939da10af2d6c2b1569261d39
Callum 🍺 <callumimacmillan@gmail.com>
2021-05-17 20:49:49 +0100

    Added player health menu

--- site/src/client/services/render/services/SpriteRenderService.ts
+++ site/src/client/services/render/services/SpriteRenderService.ts
@@ -260,11 +260,10 @@ export class SpriteRenderService implements RenderItemInterface<Sprite> {
         this.translations[t1i + 16] = y;
         this.translations[t1i + 17] = z;
 
-        const floatCorrection = 0;
-        const texX = sprite.texture.textureX + floatCorrection;
-        const texY = sprite.texture.textureY + floatCorrection;
-        const texWidth = sprite.texture.textureWidth - floatCorrection * 2;
-        const texHeight = sprite.texture.textureHeight - floatCorrection * 2;
+        const texX = sprite.texture.textureX;
+        const texY = sprite.texture.textureY;
+        const texWidth = sprite.texture.textureWidth;
+        const texHeight = sprite.texture.textureHeight;
 
         // t1
 
